85c36f88a006ea4193301e6bd24f2dab6410d2f3
Sdac <Sdac@Sdac-PC>
2023-06-14 03:56:03 +0530

    demo

---
+++ demo/patil/src/patil/spatil2.java
@@ -0,0 +1,7 @@
+package patil;
+
+public class spatil2 {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+}
---
+++ demo/patil/src/patil/test1.java
@@ -0,0 +1,7 @@
+package patil;
+
+public class test1 {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+}
